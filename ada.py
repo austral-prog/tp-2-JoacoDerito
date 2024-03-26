4b8354304eb7a6380f0fa060d8b63fd8e07f1adb
JoacoDerito <162723144+JoacoDerito@users.noreply.github.com>
2024-03-26 11:09:24 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,12 @@
 def ada():
-    first_name = "AdA"
-    last_name = "LoVeLAce"
+    first_name = "Ada "
+    second_name = "Lovelace"
+    name= first_name + second_name
+    print (name.upper())
+    print (name.lower())
+    print (first_name.lower())
+    print (first_name.upper())
+    print (second_name.lower())
+    print (second_name.upper())
+
+ada()
